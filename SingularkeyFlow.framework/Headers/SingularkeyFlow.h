11bc2df9bd02c35764f28c07bf9cd58bf3092e81
Neetin Sharma <return@neetin.com.np>
2020-10-02 23:48:17 +0545

    adds SingularKeyFlow podspec with other dependencies

---
+++ SingularkeyFlow.framework/Headers/SingularkeyFlow.h
@@ -0,0 +1,19 @@
+//
+//  SingularkeyFlow.h
+//  SingularkeyFlow
+//
+//  Created by neetin on 4/8/20.
+//  Copyright © 2020 SingularKey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SingularkeyFlow.
+FOUNDATION_EXPORT double SingularkeyFlowVersionNumber;
+
+//! Project version string for SingularkeyFlow.
+FOUNDATION_EXPORT const unsigned char SingularkeyFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SingularkeyFlow/PublicHeader.h>
+
+
